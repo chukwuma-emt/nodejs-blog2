8ebd51eb04b46d5002f165f139376bf809d97dde
chukwuma <silpenser@gmail.com>
2025-04-25 22:50:33 +0100

    commit7

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt')
 const  jwt = require('jsonwebtoken')
 
-const adminLayout = '../views/Layouts/admin';
+const adminLayout = 'Layouts/admin';
 const jwtSecret = process.env.JWT_SECRET;
 
 
